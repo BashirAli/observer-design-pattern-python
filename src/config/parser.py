faf61ee660d8128f5198f0fe0f80838bd6a8884a
BashirAli <thebashtm@gmail.com>
2024-07-29 16:04:39 +0100

    added test data + system arg parsing

--- src/config/parser.py
+++ src/config/parser.py
@@ -1,3 +1,5 @@
+import json
+
 import yaml
 from config.logger import logger
 
@@ -15,3 +17,18 @@ def read_yaml_config(file_path: str) -> dict:
         logger.warning(f"YAML file parsing failed; {ye}")
 
     return yaml_data
+
+
+def read_json_file(file_path: str) -> list:
+    json_data = []
+    try:
+        with open(file_path, 'r') as file:
+            json_data = json.load(file)
+    except FileNotFoundError as ex:
+        logger.warning(f"YAML configuration file not found in {file_path}: {ex}")
+    except PermissionError as pe:
+        logger.warning(f"YAML configuration file not found in {file_path}: {pe}")
+    except json.JSONDecodeError as de:
+        logger.warning(f"JSON file parsing failed; {de}")
+
+    return json_data
--- src/main.py
+++ src/main.py
@@ -1,10 +1,42 @@
-from config.yaml_parser import read_yaml_config
+import argparse
+
+from config.parser import read_yaml_config, read_json_file
+from config.logger import logger
+import argparse
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input_path_to_yaml',
+        type=str,
+        required=True,
+        help='Path to YAML config file.'
+    )
+
+    parser.add_argument(
+        '--input_path_to_data',
+        type=str,
+        required=True,
+        help='Path to JSON notification data.'
+    )
+
+    return parser.parse_args()
 
 
 def main():
-    observer_configuration = read_yaml_config("main.yaml")
-    print(observer_configuration)
+    system_args = arg_parser()
+    observer_configuration = read_yaml_config(system_args.input_path_to_yaml)
+    if observer_configuration:
+        # create topic and subs
+        print(observer_configuration)
+        logger.info("Created Observer infrastructure")
+        logger.info("Sending test message(s)")
 
+    dummy_data = read_json_file(system_args.input_path_to_data)
+    if dummy_data:
+        # notify subscribers 
+        print(dummy_data)
 
 
 if __name__ == "__main__":
