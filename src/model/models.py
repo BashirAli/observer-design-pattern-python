837453ef955c397d92de5e22240315ad7e4f1c8a
BashirAli <thebashtm@gmail.com>
2024-07-30 10:38:28 +0100

    added model functionality

--- src/model/models.py
+++ src/model/models.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
 from pydantic import Field, BaseModel
+from pydantic_core import ValidationError
 from datetime import datetime
-
+from config.logger import logger
 
 class Notification(BaseModel):
     data: dict = Field(..., description="dictionary of data")
@@ -21,8 +22,11 @@ class Subscriber(abstractSubscriber):
         self.subscriber_display_name = subscriber_display_name
         self.subscriber_id = subscriber_id
 
-    def acknowledge_message(self, notification):
-        print(f"Received notification {notification} with data {notification.data}")
+    def acknowledge_message(self, notification: Notification):
+        return f"{self.subscriber_display_name} has acknowledged notification {notification.notification_id} with data: {notification.data}, attributes: {notification.attributes} at {notification.notification_time}"
+
+    def __repr__(self):
+        return f"{self.subscriber_display_name}"
 
 
 class Topic:
@@ -31,12 +35,24 @@ class Topic:
         self.topic_name: str = topic_name
         self.subscribers: list[Subscriber] = []
 
-    def _validate_data(self):
-        pass
+    def __repr__(self):
+        return f"{self.topic_name}, {self.subscribers}"
+
+    @staticmethod
+    def _validate_publish(data: dict) -> Notification | None:
+        try:
+            validated_model = Notification(**data)
+        except ValidationError as ve:
+            logger.info(f"Message is invalid {ve}")
+            validated_model = None
+
+        return validated_model
 
     def add_subscriber(self, subscriber: Subscriber) -> None:
-        pass
+        self.subscribers.append(subscriber)
 
-    def publish(self, ) -> None:
-        for sub in self.subscribers:
-            sub.acknowledge_message()
+    def publish(self, data: dict) -> None:
+        publish_notification = self._validate_publish(data)
+        if publish_notification:
+            for sub in self.subscribers:
+                sub.acknowledge_message(publish_notification)
